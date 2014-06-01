e99c31d4529551cf9fa5b5659912efd7c3e3f113
Allan Yoshio Hasegawa <hasegawa.aran@gmail.com>
2014-06-01 04:28:56 -0300

    put together many pieces of various samples; new load_program function;

---
+++ src/glutils.rs
@@ -0,0 +1,106 @@
+
+extern crate native;
+extern crate glfw;
+extern crate gl;
+
+use std::ptr;
+use std::str;
+use std::io::File;
+use std::io::BufferedReader;
+
+use gl::types::*;
+
+pub static VERTEX_DATA: [GLfloat, ..6] = [
+	     0.0,  0.5,
+	     0.5, -0.5,
+	    -0.5, -0.5
+];
+
+pub fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+	let shader = gl::CreateShader(ty);
+	unsafe {
+		src.with_c_str(|ptr| gl::ShaderSource(shader, 1, &ptr, ptr::null()));
+		gl::CompileShader(shader);
+
+		let mut status = gl::FALSE as GLint;
+		gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+		if status != (gl::TRUE as GLint) {
+			let mut len = 0;
+			gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+			let mut buf = Vec::from_elem(len as uint - 1, 0u8);
+			gl::GetShaderInfoLog(shader, len, ptr::mut_null(),
+				buf.as_mut_ptr() as *mut GLchar);
+			fail!("{}", str::from_utf8(buf.as_slice()).expect(
+					"ShaderInfoLog not valid utf8"));
+		}
+	}
+	shader
+}
+
+pub fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+	let program = gl::CreateProgram();
+	gl::AttachShader(program, vs);
+	gl::AttachShader(program, fs);
+	gl::LinkProgram(program);
+	unsafe {
+		let mut status = gl::FALSE as GLint;
+		gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+		if status != (gl::TRUE as GLint) {
+			let mut len: GLint = 0;
+			gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+			let mut buf = Vec::from_elem(len as uint - 1, 0u8);
+			gl::GetProgramInfoLog(program, len, ptr::mut_null(),
+				buf.as_mut_ptr() as *mut GLchar);
+			fail!("{}", str::from_utf8(buf.as_slice()).expect(
+					"ProgramInfoLog not valid utf8"));
+		}
+	}
+	program
+}
+
+
+fn read_file(file_path: &Path, content: &mut String) -> () {
+	let mut file = BufferedReader::new(File::open(file_path));
+	for line_iter in file.lines() {
+		let line: String = match line_iter {
+			Ok(v) => v,
+			Err(e) => fail!("Error: {}", e)
+		};
+
+		content.push_str(
+			match str::from_utf8(line.as_bytes()) {
+				Some(v) => v,
+				None => ""
+			}
+		);
+	}
+}
+
+pub fn load_program(vs: &str, fs: &str) -> GLuint {
+	let vertex_path = Path::new(vs);
+	let mut vertex_shader = box String::new();
+	read_file(&vertex_path, vertex_shader);
+	let fragment_path = Path::new(fs);
+	let mut fragment_shader = box String::new();
+	read_file(&fragment_path, fragment_shader);
+
+
+	let vs = ::glutils::compile_shader(
+	match str::from_utf8(vertex_shader.as_bytes()) {
+		Some(v) => v,
+		None => ""
+	}, gl::VERTEX_SHADER);
+	let fs = ::glutils::compile_shader(
+		match str::from_utf8(fragment_shader.as_bytes()) {
+			Some(v) => v,
+			None => ""
+		}, gl::FRAGMENT_SHADER);
+
+    let program = ::glutils::link_program(vs, fs);
+	gl::DeleteShader(fs);
+	gl::DeleteShader(vs);
+
+	return program;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,83 @@
+#![feature(globs)]
 
 extern crate native;
 extern crate glfw;
+extern crate gl;
 
 use glfw::Context;
+use gl::types::*;
+use std::mem;
+use std::ptr;
+
+mod glutils;
+
+#[start]
+fn start(argc: int, argv: **u8) -> int {
+	native::start(argc, argv, main)
+}
 
 fn main() {
 
 	println!("Hello World!");
 
 	let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+	glfw.window_hint(glfw::ContextVersion(3, 2));
+    glfw.window_hint(glfw::OpenglForwardCompat(true));
 	
-	let (window, events) = glfw.create_window(300, 300,
+	let (window, events) = glfw.create_window(800, 600,
 		"Title :)", glfw::Windowed).expect("Failed to create GLFW window.");
 
 	window.set_key_polling(true);
 	window.make_current();
+
+	gl::load_with(|s| glfw.get_proc_address(s));
+
+	let program = ::glutils::load_program("shaders/0.vert", "shaders/0.frag");
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+	unsafe {
+		gl::GenVertexArrays(1, &mut vao);
+		gl::BindVertexArray(vao);
+
+		gl::GenBuffers(1, &mut vbo);
+		gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+		gl::BufferData(gl::ARRAY_BUFFER,
+			(::glutils::VERTEX_DATA.len()
+				* mem::size_of::<GLfloat>()) as GLsizeiptr,
+			mem::transmute(&::glutils::VERTEX_DATA[0]),
+			gl::STATIC_DRAW);
+
+		gl::UseProgram(program);
+		"out_color".with_c_str(
+			|ptr| gl::BindFragDataLocation(program, 0, ptr));
+		let pos_attr = 
+			"position".with_c_str(|ptr| gl::GetAttribLocation(program, ptr));
+		gl::EnableVertexAttribArray(pos_attr as GLuint);
+		gl::VertexAttribPointer(pos_attr as GLuint, 2, gl::FLOAT,
+			gl::FALSE as GLboolean, 0, ptr::null());
+	}
 	
 	while !window.should_close() {
 		glfw.poll_events();
 		for (_, event) in glfw::flush_messages(&events) {
 			handle_window_event(&window, event);
 		}
+		gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+		gl::Clear(gl::COLOR_BUFFER_BIT);
+
+		gl::DrawArrays(gl::TRIANGLES, 0, 3);
+
+		// Swap buffers
+		window.swap_buffers();
+	}
+
+	gl::DeleteProgram(program);
+	unsafe {
+		gl::DeleteBuffers(1, &vbo);
+		gl::DeleteVertexArrays(1, &vao);
 	}
 }
 
